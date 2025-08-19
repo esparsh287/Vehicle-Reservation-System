b252b6751bced87e11d7df785f8e039bb77a5fa2
esparsh287 <esparsh.tamrakar18@gmail.com>
2025-08-19 18:05:40 +0545

    vehicles added, home page formatted and categories added

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.dashboard, name='home')
+    path('', views.dashboard, name='home'),
+    path('about/', views.about, name='about'),
+    path('categories/<str:name>/', views.categories, name='categories')
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.models import Vehicle
 
 
-#@login_required
+@login_required
 def dashboard(request):
     if hasattr(request.user, 'vehicles'):
         pending_vehicles = request.user.vehicles.filter(is_active=False)
         if pending_vehicles.exists():
             return render(request, 'accounts/kyc_wait.html')
+    
+    veheicles=Vehicle.objects.all()
+    return render(request, 'app/dashboard.html', {'vehicles':veheicles}) 
 
-    return render(request, 'app/dashboard.html') 
+
+def about(request):
+    return render(request, 'app/about.html',{})
+
+def categories(request, name):
+    vehicles=Vehicle.objects.filter(vehicle_type=name.lower())
+    if name == 'car':
+        return render(request, 'app/car.html',{'vehicles': vehicles})
+    elif name == 'van':
+        return render(request, 'app/van.html',{'vehicles': vehicles})
+    elif name == 'bus':
+        return render(request, 'app/bus.html',{'vehicles': vehicles})
+    else:
+        return render(request, 'app/truck.html',{'vehicles': vehicles})
+    
+    
